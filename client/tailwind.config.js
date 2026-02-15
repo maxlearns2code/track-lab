b7d8bbb2a1c586e6819033e52d050cea0eb7c117
Maxime Dewynter <maxdevvynter@gmail.com>
2026-02-15 20:59:13 +0100

    chore: add build script and ignore tsbuildinfo

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -59,25 +59,10 @@ export default {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
-        blob: {
-          "0%": { // Start
-            transform: "translate(0px, 0px) scale(1)",
-          },
-          "33%": { // Move and grow
-            transform: "translate(30px, -50px) scale(1.1)",
-          },
-          "66%": { // Move opposite and shrink
-            transform: "translate(-20px, 20px) scale(0.9)",
-          },
-          "100%": { // Back to start
-            transform: "translate(0px, 0px) scale(1)",
-          },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        blob: "blob 7s infinite",
       },
     },
   },
