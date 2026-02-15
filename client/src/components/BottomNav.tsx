42915c274082f176680bba6f00be8049ab982c8a
Maxime Dewynter <maxdevvynter@gmail.com>
2026-02-15 21:09:06 +0100

    feat(ui): add micro-interactions to bottom nav

--- client/src/components/BottomNav.tsx
+++ client/src/components/BottomNav.tsx
@@ -5,10 +5,10 @@ export function BottomNav() {
   return (
     <div className="fixed bottom-0 left-0 right-0 p-4 bg-white/10 backdrop-blur-md border-t border-white/20 z-10">
       <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="explore">
+        <TabsTrigger value="explore" className="transition-all hover:bg-white/5 data-[state=active]:bg-white/10 active:scale-95">
           <Search className="h-4 w-4 mr-2" /> Explore
         </TabsTrigger>
-        <TabsTrigger value="favorites">
+        <TabsTrigger value="favorites" className="transition-all hover:bg-white/5 data-[state=active]:bg-white/10 active:scale-95">
           <Heart className="h-4 w-4 mr-2" /> Library
         </TabsTrigger>
       </TabsList>
